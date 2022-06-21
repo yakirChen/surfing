5262a9aa96aab08c6cded7370066a1ef80c47a14
yakirChen <iamyakirchen@outlook.com>
2022-06-21 17:00:42 +0800

    函数

--- swift/Package.swift
+++ swift/Package.swift
@@ -12,26 +12,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "hello"
-            ),
-        .executableTarget(
-            name: "basic"
-            ),
-        .executableTarget(
-            name: "character"
-            ),
-        .executableTarget(
-            name: "collection"
-            ),
-        .executableTarget(
-            name: "controlFlow"
-            ),
-        .executableTarget(
-            name: "functions"
-            )
-        // .testTarget(
-        //     name: "swiftTests",
-        //     dependencies: ["swift"]),
+        .executableTarget(name: "hello"),
+        .executableTarget(name: "basic"),
+        .executableTarget(name: "character"),
+        .executableTarget(name: "collection"),
+        .executableTarget(name: "controlFlow"),
+        .executableTarget(name: "functions"),
+        .executableTarget(name: "closures")
+        // .testTarget(name: "swiftTests", dependencies: ["swift"]),
     ]
 )
